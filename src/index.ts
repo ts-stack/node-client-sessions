a87e0a7910427242f228d80ffa5fa99f0c2d9406
KostyaTretyak <ktretiak.in.ua@gmail.com>
2020-04-20 10:44:36 +0300

    Added export types to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { clientSessionFactory as sessions } from './client-session-factory';
 export { encode, decode, computeHmac } from './util';
+export { Opts as ClientSessionOptions, Cookie } from './types';
